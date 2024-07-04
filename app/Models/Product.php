19f580e06c4e714cf3d047ca7efdef27f4dd91a0
coremraz <50779187+coremraz@users.noreply.github.com>
2024-07-04 13:38:08 +0300

    Tables structure update

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -36,4 +36,9 @@ class Product extends Model
         return $this->belongsTo(Category::class, 'category_id');
     }
 
+    public function currency()
+    {
+        return $this->belongsTo(Currency::class);
+    }
+
 }
--- database/migrations/2024_07_04_085203_create_order_compositions_table.php
+++ database/migrations/2024_07_04_085203_create_order_compositions_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Order;
+use App\Models\Product;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,8 +13,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('products', function (Blueprint $table) {
-            $table->foreignIdFor(\App\Models\Exchange_rate::class);
+        Schema::create('order_compositions', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->timestamps();
         });
     }
 
@@ -21,8 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('products', function (Blueprint $table) {
-            $table->foreignIdFor(\App\Models\Exchange_rate::class);
-        });
+        Schema::dropIfExists('order_compositions');
     }
 };
