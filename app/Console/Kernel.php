52cb01d5b79e5bb714c485e9e01374ea5786b1de
Coremraz <reason1337@mail.ru>
2024-07-09 14:00:37 +0300

    exchange rate every minute test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,29 +1,27 @@
 <?php
+
 namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use App\Console\Commands\FetchExchangeRates;
 
 class Kernel extends ConsoleKernel
 {
     /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        FetchExchangeRates::class,
-    ];
-
-    /**
      * Define the application's command schedule.
-     *
-     * @param Schedule $schedule
-     * @return void
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command(FetchExchangeRates::class)->everyFiveSeconds();
+        $schedule->command('fetch:exchange-rates')->hourly();
+    }
+
+    /**
+     * Register the commands for the application.
+     */
+    protected function commands(): void
+    {
+        $this->load(__DIR__.'/Commands');
+
+        require base_path('routes/console.php');
     }
 }
