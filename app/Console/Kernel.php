58031c95150f431eb738a0426797d111e364565b
Coremraz <reason1337@mail.ru>
2024-07-09 11:53:00 +0300

    exchanges command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,29 +1,29 @@
-    <?php
-    namespace App\Console;
+<?php
+namespace App\Console;
 
-    use Illuminate\Console\Scheduling\Schedule;
-    use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-    use App\Console\Commands\FetchExchangeRates;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Console\Commands\FetchExchangeRates;
 
-    class Kernel extends ConsoleKernel
+class Kernel extends ConsoleKernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        FetchExchangeRates::class,
+    ];
+
+    /**
+     * Define the application's command schedule.
+     *
+     * @param Schedule $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule): void
     {
-        /**
-         * The Artisan commands provided by your application.
-         *
-         * @var array
-         */
-        protected $commands = [
-            FetchExchangeRates::class,
-        ];
-
-        /**
-         * Define the application's command schedule.
-         *
-         * @param Schedule $schedule
-         * @return void
-         */
-        protected function schedule(Schedule $schedule): void
-        {
-            $schedule->command(FetchExchangeRates::class)->everyMinute();
-        }
+        $schedule->command(FetchExchangeRates::class)->everyMinute();
     }
+}
