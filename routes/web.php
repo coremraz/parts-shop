a887f7b1d4b0bfd359eea406fe639b40bc620d66
Coremraz <reason1337@mail.ru>
2024-07-17 14:39:28 +0300

    complete profile functionality

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 
 //регистрация
     Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
-    Route::post('/register', [RegisteredUserController::class, 'store'])->name('register');
+    Route::post('/register', [RegisteredUserController::class, 'store'])->name('register.store');
     Route::get('/login', [SessionController::class, 'create'])->name('login');
     Route::post('/login', [SessionController::class, 'store'])->name('login');
     Route::get('/logout', [SessionController::class, 'destroy'])->name('logout');
